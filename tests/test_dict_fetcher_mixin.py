5c8071cb01cc44cf8581bf1b79e1d32f5bf059bf
Yang <zhy0216@gmail.com>
2017-03-12 17:02:33 -0700

    add more test cases

--- tests/test_dict_fetcher_mixin.py
+++ tests/test_dict_fetcher_mixin.py
@@ -35,20 +35,29 @@ df = DictFetcher(d)
 def test_simple_dot_expression():
     assert df["c.d.f.g"]["c.d.f.g"] == "1234"
 
+def test_simple_dot_expty_expression():
+    assert df["c.a.f.g"] == {}
 
 def test_simple_star_expression():
     assert is_dict_same(df["a.b.ab*"], {"a.b.abc": "what", "a.b.aba": "the"})
 
 
 def test_question_mark_expression():
-    pass
+    assert is_dict_same(df["a.?.x"], {"a.c.x": "bug"})
 
 def test_char_choose_expression():
-    pass
+    assert is_dict_same(df["a.b.ab[abc]"], {"a.b.abc": "what", "a.b.aba": "the"})
+    assert is_dict_same(df["a.b.ab[ab]"], {"a.b.aba": "the"})
 
+
+def test_key_has_dot_expression():
+    assert is_dict_same(df["a.c.x.y"], {})
+
+def test_list_expression():
+    assert is_dict_same(df[("a", "c", "x.y")], {"a.c.x.y": "except"})
 
 def test_empty_result():
-    pass
+    assert df["not"] == {}
 
 def is_dict_same(d1, d2):
     return sorted(d1) == sorted(d2)
