ef88b707494bd3d41fb6e7136fb7b62c2db0d596
Yang <zhy0216@gmail.com>
2017-03-12 20:41:42 -0700

    upate a little on heap

--- README.md
+++ README.md
@@ -4,3 +4,11 @@ Some little programs (useeless?) based on CPython 2.7
 Doc: see the test cases under test/ folder
 
 > it's a matter of taste
+
+
+something maybe interesting: (something I have not understanded...)
+* Aho-Corasick Algorithm:
+    - http://www.geeksforgeeks.org/aho-corasick-algorithm-pattern-searching/
+    - http://www.cs.uku.fi/~kilpelai/BSA05/lectures/slides04.pdf
+* Manacher’s Algorithm
+    - http://www.geeksforgeeks.org/manachers-algorithm-linear-time-longest-palindromic-substring-part-1/
--- heap.py
+++ heap.py
@@ -1,10 +1,20 @@
 import heapq
 
+# original heap implementation: 
+# https://hg.python.org/cpython/file/2.7/Lib/heapq.py
+
+## resource: https://www.youtube.com/watch?v=WsNQuCa_-PU
 
 class Heap:
     def __init__(self, array, key=lambda x: x):
-        pass
         self._key = key
+        self.array = array
+
+    def _siftdown(self, startpos, pos):
+        pass
+
+    def _siftup(self, pos):
+        pass
 
     def push(self, x):
         pass
@@ -13,5 +23,13 @@ class Heap:
         pass
 
     def heapify(self, key=None):
+        pass
+
+
+    ## https://docs.python.org/2.7/reference/datamodel.html
+    def __len__(self):
+        return len(self.array)
+
+    def __getitem__(self, key):
         pass
 
