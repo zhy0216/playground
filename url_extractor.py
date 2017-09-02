8cd788a94e0beb3645cd3e8bdcc86f7f03807121
Yang <zhy0216@gmail.com>
2017-09-01 22:12:11 -0700

    add url extractor simple implementation

--- url_extractor.py
+++ url_extractor.py
@@ -1,3 +1,4 @@
+import re
 
 
 class UrlExtractor:
@@ -6,11 +7,19 @@ class UrlExtractor:
     match => self.named_re march
     extract => group match to dict
     '''
-    def __init__(self, pattern):
-        pass
+    def __init__(self, pattern: str):
+        self._pattern = pattern
+        self._meta_pattern = self._parse_pattern()
+
+    def _parse_pattern(self):
+        meta_pattern = re.sub('{([\w_]+)}', r'(?P<\1>[\w_]+)', self._pattern)
+        return re.compile(meta_pattern)
 
     def match(self, url):
-        pass
+        return self._meta_pattern.fullmatch(url)
 
     def extract(self, url):
-        pass
+        matched = self._meta_pattern.match(url)
+        if matched:
+            return matched.groupdict()
+        return {}
